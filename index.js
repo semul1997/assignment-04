b72e4b1967ef9b6954d01739f0778272f4cba1e6
Semul <msrahman115500@gmail.com>
2024-09-23 22:09:40 +0600

    added new another html file and redirected functionality added

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ let historyButton = getElement('history');
 let donateQuota = getElement('donate-quota');
 let mainContainer = getElement('main-container')
 let donationHistory = getElement('donation-history')
-
-
+let modal = getElement('open-modal')
+let closeModal = getElement('modal-close');
+// donaation for Noakhali
 
 donateNoakhali.addEventListener('click', function(){
     let noakhaliDonation = parseFloat(getElement('noakhali-donation').innerText)
@@ -27,7 +28,8 @@ donateNoakhali.addEventListener('click', function(){
  
     let inputNoakhali = parseFloat(getElement('input-noakhali').value);
     if(inputNoakhali < 0 || isNaN(inputNoakhali)){
-        alert('please enter valid input')
+        getElement('input-noakhali').value = "";
+        return alert('please enter valid amount')
     }
     
     let newAmount = inputNoakhali + noakhaliDonation;
@@ -50,12 +52,100 @@ donateNoakhali.addEventListener('click', function(){
 
 })
 
-let modal = getElement('open-modal')
-let closeModal = getElement('modal-close');
+
 closeModal.addEventListener('click', function(){
     
     modal.classList.add('hidden')
 })
+
+
+// Donation for feni
+
+donateFeni.addEventListener('click', function(){
+    let feniDonation = parseFloat(getElement('feni-donation').innerText)
+    let reminingBalance = parseFloat(getElement('remaining-balance').innerText);
+    let donationHistory = getElement('donation-history')
+    
+ 
+    let inputFeni = parseFloat(getElement('input-feni').value);
+    if(inputFeni< 0 || isNaN(inputFeni)){
+        getElement('input-feni').value = "";
+       return alert('please enter valid amount')
+    }
+    
+    let newAmount = inputFeni + feniDonation;
+    getElement('feni-donation').innerText = newAmount;
+
+    getElement('remaining-balance').innerText = reminingBalance - inputFeni;
+    getElement('input-feni').value = "";
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "my-5 border p-5 rounded-md"
+    
+    newDiv.innerHTML =  `
+                        <p  class = "font-bold"> ${inputFeni} Taka is Donated For flood relief in 2024 at  Feni, Bangladesh </p> 
+                        <p> ${new Date()} </p>
+                        `;
+    donationHistory.insertBefore(newDiv, donationHistory.firstChild)
+
+    let modal = getElement('open-modal')
+    modal.classList.remove('hidden')
+
+})
+
+
+closeModal.addEventListener('click', function(){
+    
+    modal.classList.add('hidden')
+})
+
+
+// donation for quota-injured
+
+
+donateQuota.addEventListener('click', function(){
+    let quotaDonation = parseFloat(getElement('quota-donation').innerText)
+    let reminingBalance = parseFloat(getElement('remaining-balance').innerText);
+    let donationHistory = getElement('donation-history')
+    
+ 
+    let inputquota = parseFloat(getElement('input-quota').value);
+    if(inputquota < 0 || isNaN(inputquota)){
+        getElement('input-quota').value = "";
+       return alert('please enter valid amount')
+    }
+    
+    let newAmount = inputquota + quotaDonation;
+    getElement('quota-donation').innerText = newAmount;
+
+    getElement('remaining-balance').innerText = reminingBalance - inputquota;
+    getElement('input-quota').value = "";
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "my-5 border p-5 rounded-md"
+    
+    newDiv.innerHTML =  `
+                        <p  class = "font-bold"> ${inputquota} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh </p> 
+                        <p> ${new Date()} </p>
+                        `;
+    donationHistory.insertBefore(newDiv, donationHistory.firstChild)
+
+    let modal = getElement('open-modal')
+    modal.classList.remove('hidden')
+
+})
+
+
+closeModal.addEventListener('click', function(){
+    
+    modal.classList.add('hidden')
+})
+
+
+
+
+
+// history button functionality 
 
 
 historyButton.addEventListener('click', function(){
@@ -69,6 +159,8 @@ historyButton.addEventListener('click', function(){
 
 })
 
+// donation Button functionality
+
 donationButton.addEventListener('click', function(){
     mainContainer.classList.remove('hidden')
     historyButton.classList.add('border', 'bg-transparent','border-inputBorder')
@@ -77,5 +169,21 @@ donationButton.addEventListener('click', function(){
     donationButton.classList.remove('border', 'bg-transparent','border-inputBorder')
 
     donationHistory.classList.add('hidden')
+
+})
+
+// blog button functionality
+
+blogButton.addEventListener('click', function(){
+    window.location.href = "./home.html"
+})
+
+// navbar scroll function 
+
+let navbar = getElement('navbar');
+console.log(navbar)
+navbar.addEventListener('scroll', function(){
+    navbar.classList.remove('bg-navBg')
+    navbar.classList.add('bg-black')
 
 })
